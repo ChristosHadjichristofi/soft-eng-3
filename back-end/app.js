c2704bee4d6599cee8649a8150915c7f9efa62e6
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2021-01-09 22:33:44 +0200

    update project hierarchy

--- back-end/app.js
+++ back-end/app.js
@@ -1,11 +1,9 @@
 const http = require('http');
-
 const express = require('express');
-
 const app = express();
-
+const port = process.env.PORT || 8765;
 
 
 const server = http.createServer(app);
 
-server.listen(8765);
+server.listen(port, () => console.log(`Listening on port ${port}`));
