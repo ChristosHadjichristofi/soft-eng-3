b623bb711832c0be0c2bcc2a64fb7d2ae604bc55
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2021-01-15 16:12:53 +0200

    Complete populate-db

--- back-end/util/populate-db.js
+++ back-end/util/populate-db.js
@@ -5,32 +5,6 @@ const data_importer = require('./data_importer');
 
 function populate() {
     
-    // data_importer("./data/administrators.csv", models.administrators, true)
-    // .then (function () {
-    //     data_importer("./data/charging-stations.csv", models.charging_stations, false)
-    //     .then (function () {
-    //         data_importer("./data/owners.csv", models.owners, true)
-    //         .then (function () {
-    //             data_importer("./data/energy-providers.csv", models.energy_providers, false)
-    //             .then (function() {
-    //                 data_importer("./data/charging-points.csv", models.charging_points, false)
-    //                 .then (function() {
-    //                     data_importer("./data/supported-cars.csv", models.supported_cars, false)
-    //                     .then (function() {
-    //                         data_importer("./data/registered-cars.csv", models.registered_cars, false)
-    //                         .then (function() {
-    //                             data_importer("./data/driven-by.csv", models.driven_by, false)
-    //                             .then (function() {
-    //                                 data_importer("./data/sessions.csv", models.sessions, false)
-    //                             })
-    //                         })
-    //                     })
-    //                 })
-    //             })
-    //         })
-    //     })
-    // })
-
     data_importer("./data/administrators.csv", models.administrators, true)
     .then( () => {
         return data_importer("./data/charging-stations.csv", models.charging_stations, false)
@@ -53,17 +27,9 @@ function populate() {
     .then( () => {
         return data_importer("./data/driven-by.csv", models.driven_by, false)
     })
-    // .then( () => {
-    //     return data_importer("./data/sessions.csv", models.sessions, false)
-    // })
-    
-    
-    
-    
-    
-    
-    
-
+    .then( () => {
+        return data_importer("./data/sessions.csv", models.sessions, false)
+    })
 }
 
 module.exports = populate;
