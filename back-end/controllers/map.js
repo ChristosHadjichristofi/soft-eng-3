41c111c87b0ede0a599e649ce0ae4fae01286258
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2021-01-19 12:41:53 +0200

    Add csv format response to maps.js/getShowMap controller

--- back-end/controllers/map.js
+++ back-end/controllers/map.js
@@ -9,6 +9,8 @@ const op = Sequelize.Op;
 
 exports.getShowMap = (req, res, next) => {
 
+    const format = req.query.format;
+
     models.charging_stations.findAll({
         raw: true,
         order: [
@@ -22,15 +24,29 @@ exports.getShowMap = (req, res, next) => {
         ]
     })
     .then(stations => {
-        console.log(stations)
         if (stations.length == 0) {
             return res.status(402).json({ message: "No data found!" })
         }
 
-        res.status(201).json({
-            RequestTimestamp: new Date(),
-            StationsList: stations
-        })
+        if (format == 'csv') {
+            var updStationRecord;
+            var stationsArr = [];
+            stations.forEach(stationRecord => {
+                updStationRecord = {
+                    RequestTimestamp: new Date(),
+                    ...stationRecord
+                }
+                stationsArr.push(updStationRecord);
+            })
+            res.csv(stationsArr, 200);
+
+        }
+        else {
+            res.status(201).json({
+                RequestTimestamp: new Date(),
+                StationsList: stations
+            })
+        }
     })
     .catch (err => {
         console.log(err)
