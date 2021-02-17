4a256064abcbc370c16ae70a03f27744e8cf2254
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2021-02-17 18:15:15 +0200

    Add permissions for admin routes

--- back-end/routes/admin.js
+++ back-end/routes/admin.js
@@ -3,21 +3,22 @@ const express = require('express');
 const adminController = require('../controllers/admin');
 
 const upload = require("../middlewares/upload");
-const isAuth = require('../middlewares/auth')
+const isAuth = require('../middlewares/authentication')
+const permit = require('../middlewares/authorization')
 
 const router = express.Router();
 
 
-router.post('/usermod/:username/:password', isAuth, adminController.postUsermod);
+router.post('/usermod/:username/:password', isAuth, permit('sysadmin'), adminController.postUsermod);
 
 router.post('/login', adminController.login);
 
-router.get('/users/:username', isAuth, adminController.getUser);
+router.get('/users/:username', isAuth, permit('sysadmin'), adminController.getUser);
 
-router.post('/system/sessionsupd', upload.single("file"), adminController.postSystem);
+router.post('/system/sessionsupd', isAuth, permit('sysadmin'), upload.single("file"), adminController.postSystem);
 
-router.get('/healthcheck', isAuth, adminController.getHealthcheck);
+router.get('/healthcheck', isAuth, permit('sysadmin'), adminController.getHealthcheck);
 
-router.post('/resetsessions', isAuth, adminController.postResetsessions);
+router.post('/resetsessions', isAuth, permit('sysadmin'), adminController.postResetsessions);
 
 module.exports = router;
