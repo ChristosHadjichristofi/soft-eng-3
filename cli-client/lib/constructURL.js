cdc05b94d1d2761af0d9777dcb4c13f06fcf379b
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2021-02-24 20:45:48 +0200

    Finish constructURL.js (cli)

--- cli-client/lib/constructURL.js
+++ cli-client/lib/constructURL.js
@@ -1,9 +1,23 @@
 module.exports = function (scope, param1, param2, param3, format, apikey) {
     let base = 'http://localhost:8765/evcharge/api';
-    base = base + type;
+    // create url with scope
+    base = base + scope;
 
+    // create url for SessionsPerPoint, SessionsPerStation, SessionsPerEV, SessionsPerProvider
     if (scope === '/SessionsPerPoint/' || scope === '/SessionsPerStation/' || scope === '/SessionsPerEV/' || scope === '/SessionsPerProvider/') {
         base = base + param1 + '/' + param2 + '/' + param3 + '?format=' + format;
     }
-    else if (scope === '/admin/')
+    // create url for Admin scope
+    // healthcheck 
+    // resetsessions
+    // usermod /admin/usermod/param1/param2 -> param1 = usermod, param2 = username, param3 = password
+    // users
+    // sessionsupd /admin/system/sessionsupd/
+    else if (scope === '/admin/') {
+        if (param1 === 'sessionsupd') base = base + 'system/' + param1;
+        else if (param1 === 'usermod') base = base + param1 + '/' + param2 + '/' + param3;
+        else if (param1 === 'users') base = base + param1 + '/' + param2;
+        else base = base + param1;
+    }
+    return base;
 }
