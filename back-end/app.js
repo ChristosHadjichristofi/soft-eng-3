0e93da060e70ca2c344e3249520a5dda7e9aa455
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2021-03-02 22:20:22 +0200

    Fix Access-Control-Allow-Headers

--- back-end/app.js
+++ back-end/app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type', 'Authorization', 'X-OBSERVATORY-AUTH');
+    res.setHeader('Access-Control-Allow-Headers', "Content-Type, Authorization, X-OBSERVATORY-AUTH");
     next();
 });
 
