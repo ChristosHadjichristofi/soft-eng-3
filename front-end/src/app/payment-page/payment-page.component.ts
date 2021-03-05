292a38008ee531702210f484b539a93a05e6b48a
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2021-03-05 18:23:52 +0200

    Complete Payment component

--- front-end/src/app/payment-page/payment-page.component.ts
+++ front-end/src/app/payment-page/payment-page.component.ts
@@ -1,5 +1,18 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Services } from '../providers/services';
+
+interface SessionData {
+  ConnectionTime: string;
+  DisconnectionTime: string;
+  KWhDelivered: number;
+  Points: string;
+  Protocols: string;
+  Stations: string;
+  UserVehicles: string;
+}
 
 @Component({
   selector: 'app-payment-page',
@@ -8,9 +21,31 @@ import { Services } from '../providers/services';
 })
 export class PaymentPageComponent implements OnInit {
 
-  sessionObj: {}
+  sessionData: SessionData;
+  costPerkWh: string;
+  totalCost: string;
 
-  constructor(public services: Services) { }
+  constructor(public services: Services, public http: HttpClient) {
+    this.sessionData = JSON.parse(this.services.decrypt(localStorage.getItem("SessionData")));
+    localStorage.removeItem("SessionData");
+    console.log(this.sessionData);
 
-  ngOnInit(): void {}
+    
+
+    let costPerkWhUrl = 'http://localhost:8765/evcharge/api/charge/costperkwh/' + this.sessionData.Points;
+
+    this.http.get<{ costperkwh: string }>(costPerkWhUrl, { headers: this.services.getAuthHeaders() }).subscribe(result => {
+      this.costPerkWh = result.costperkwh;
+
+      let costUrl = 'http://localhost:8765/evcharge/api/charge/cost/' + this.sessionData.ConnectionTime
+        + '/' + this.sessionData.DisconnectionTime + '/' + this.sessionData.Protocols + '/' + this.costPerkWh
+
+      this.http.get<{ kWhDelivered: string, cost: string }>(costUrl, { headers: this.services.getAuthHeaders() }).subscribe(result => {
+        this.totalCost = result.cost;
+      });
+    });
+  }
+
+  ngOnInit(): void {
+  }
 }
