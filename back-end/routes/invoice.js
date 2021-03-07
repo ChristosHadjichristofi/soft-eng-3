73f0c411536795a4d69b048fff78103f2002dc8b
demKyr <demetris_k@hotmail.com>
2021-03-07 17:35:35 +0200

    Add auth to invoice

--- back-end/routes/invoice.js
+++ back-end/routes/invoice.js
@@ -7,13 +7,9 @@ const permit = require('../middlewares/authorization')
 
 const router = express.Router();
 
-router.get('/costenergytotals/:ownerid/:year/:month', invoiceController.getCostenergytotals);
+router.get('/costenergytotals/:ownerid/:year/:month', isAuth, permit('owner'), invoiceController.getCostenergytotals);
 
-router.get('/chargeslist/:ownerid/:year/:month', invoiceController.getChargeslist);
-
-// router.get('/costenergytotals/:ownerid/:year/:month', isAuth, permit('owner'), invoiceController.getCostenergytotals);
-
-// router.get('/chargeslist/:ownerid/:year/:month', isAuth, permit('owner'), invoiceController.getChargeslist);
+router.get('/chargeslist/:ownerid/:year/:month', isAuth, permit('owner'), invoiceController.getChargeslist);
 
 
 module.exports = router;
