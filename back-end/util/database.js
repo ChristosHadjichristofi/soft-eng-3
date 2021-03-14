bf59eddcfe2585ebce02789bdc0ee6cac5adbf4e
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2021-03-14 20:05:42 +0200

    Add test database

--- back-end/util/database.js
+++ back-end/util/database.js
@@ -1,16 +1,33 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('soft-eng-3', 'root', 'soft-eng-3', {
-    dialect: 'mysql',
-    protocol: 'mysql',
-    host: 'localhost',
-    port: '3306',
-    // logging: false
-});
+let sequelize;
+
+if (process.env.NODE_ENV == undefined) process.env.NODE_ENV = 'run'
+
+if (process.env.NODE_ENV.trim() === 'test') {
+    sequelize = new Sequelize('soft-eng-3-test', 'root', 'soft-eng-3', {
+        dialect: 'mysql',
+        protocol: 'mysql',
+        host: 'localhost',
+        port: '3306',
+        logging: false
+    });
+}
+else {
+    sequelize = new Sequelize('soft-eng-3', 'root', 'soft-eng-3', {
+        dialect: 'mysql',
+        protocol: 'mysql',
+        host: 'localhost',
+        port: '3306',
+        logging: false
+    });
+}
+
+
 
 sequelize.authenticate()
 .then(() => {
-    console.log("Success connecting to databse!");    
+    console.log("Success connecting to database!");    
 })
 .catch(err => {
     console.error("Unable to connect to the database", err);
